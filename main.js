5547f5bfad78f7526bce0d4cbc5b7c68ee0be332
kamalyadav11 <kyzx11@gmail.com>
2018-09-01 12:43:44 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const SHA256 = require("crypto-js/sha256");
+class Node {
+    constructor(nodeNumber, timestamp, data, previousHash = '', referenceNodeId) {
+        this.nodeNumber = nodeNumber;
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.hash = this.calculateHash();
+        this.referenceNodeId = referenceNodeId; 
+    }
+
+    calculateHash() {
+        return SHA256(this.nodeNumber + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+class GenesisNode{
+  constructor() {
+      this.chain = [this.createGenesisNode()];
+  }
+
+  createGenesisNode() {
+      return new Node(0, "01/01/2017", "Genesis block", "0");
+  }
+
+  getLatestNode() {
+      return this.chain[this.chain.length - 1];
+  }
+
+  addNode(newNode) {
+      newNode.previousHash = this.getLatestNode().hash;
+      newNode.hash = newNode.calculateHash();
+      this.chain.push(newNode);
+  }
+  
+}
+
+let kamalCoin = new GenesisNode();
+kamalCoin.addNode(new Node(1, "20/07/2017", { amount: 4 }));
+kamalCoin.addNode(new Node(2, "20/07/2017", { amount: 8 }));
+
+console.log(JSON.stringify(kamalCoin, null, 4));
